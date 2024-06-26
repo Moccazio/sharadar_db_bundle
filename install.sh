5ded21f173f43596192c8c5358b21e8945e99607
Maximilian Wimmer <71945683+Moccazio@users.noreply.github.com>
2024-06-26 15:55:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-sudo dnf install -y systemd-devel python3.9
+#conda create -n py39 -y -c conda-forge python=3.9 pandas numpy
+conda create -n py39 python=3.9 pandas numpy jupyterlab jupyter notebook -y
+source activate py39
 
-export PYTHON_VERSION=3.9
-export VENV_NAME=zipline-reloaded-venv$PYTHON_VERSION
-virtualenv -p /usr/bin/python$PYTHON_VERSION ~/$VENV_NAME
-source ~/$VENV_NAME/bin/activate
+# mkdir ~/python_libs
+
 python -m pip install --upgrade pip
-export PYTHON_LIBS=~/$VENV_NAME/lib/python$PYTHON_VERSION/site-packages
+export PYTHON_LIBS=~/miniconda3/envs/py39/lib 
+
+#export PYTHON_LIBS=~/python_libs
 
 # Install TA LIB
 cd $PYTHON_LIBS
@@ -16,7 +18,14 @@ rm ta-lib-0.4.0-src.tar.gz
 cd ta-lib
 ./configure
 make
-sudo make install
+#sudo make install
+
+# Install zipline-reloaded with source code
+pip install zipline-reloaded # for dependecies
+git clone git@github.com:stefan-jansen/zipline-reloaded.git
+cd zipline-reloaded
+python setup.py build_ext --inplace
+python setup.py install
 
 # Install TWS api
 cd $PYTHON_LIBS
@@ -24,25 +33,21 @@ wget https://interactivebrokers.github.io/downloads/twsapi_macunix.1025.01.zip
 unzip twsapi_macunix.1025.01.zip -d twsapi
 rm twsapi_macunix.1025.01.zip
 cd twsapi/IBJts/source/pythonclient
-pip install wheel
-python setup.py sdist
-python setup.py bdist_wheel
-pip install --upgrade dist/ibapi-10.25.1-py3-none-any.whl
+python setup.py install
 
 # Install sharadar_db_bundle and its requirements
 cd $PYTHON_LIBS
-git clone git@github.com:alphaville76/algo.git
-git clone git@github.com:alphaville76/sharadar_db_bundle.git
-cd $PYTHON_LIBS/sharadar_db_bundle
+git clone https://github.com/Moccazio/sharadar_db_bundle
+#git clone  https://github.com/alphaville76/sharadar_db_bundle.git
+cd $PYTHON_LIBS/sharadar-zipline
 pip install -r requirements.txt
-python setup.py sdist
-python setup.py bdist_wheel
-pip install --upgrade --force-reinstall dist/sharadar_db_bundle-2.0-py3-none-any.whl
-python test/basic_pipeline_sep_db.py
+python setup.py install
 
 if [ "$?" -eq 0 ]
 then
 echo "INSTALLATION SUCCESSFUL"
 else
 echo "INSTALLATION FAILED"
+source deactivate py39
+conda remove --name py39 --all -y
 fi
