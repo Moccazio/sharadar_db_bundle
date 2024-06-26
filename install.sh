d56fad892bd407bc3635a51d322b2321d9451721
Maximilian Wimmer <71945683+Moccazio@users.noreply.github.com>
2024-06-26 16:23:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #conda create -n py39 -y -c conda-forge python=3.9 pandas numpy
-conda create -n py39 python=3.9 pandas numpy jupyterlab jupyter notebook logbook -y
-source activate py39
+conda create -n py310 python=3.10 pandas numpy -y
+source activate py310
 
 # mkdir ~/python_libs
 
 python -m pip install --upgrade pip
-export PYTHON_LIBS=~/miniconda3/envs/py39/lib 
+export PYTHON_LIBS=~/miniconda3/envs/py310/lib 
 
 #export PYTHON_LIBS=~/python_libs
 
@@ -18,7 +18,13 @@ rm ta-lib-0.4.0-src.tar.gz
 cd ta-lib
 ./configure
 make
-sudo make install
+
+# Install zipline-reloaded with source code
+pip install zipline-reloaded # for dependecies
+git clone git@github.com:stefan-jansen/zipline-reloaded.git
+cd zipline-reloaded
+python setup.py build_ext --inplace
+python setup.py install
 
 # Install TWS api
 cd $PYTHON_LIBS
@@ -26,26 +32,20 @@ wget https://interactivebrokers.github.io/downloads/twsapi_macunix.1025.01.zip
 unzip twsapi_macunix.1025.01.zip -d twsapi
 rm twsapi_macunix.1025.01.zip
 cd twsapi/IBJts/source/pythonclient
-pip install wheel
-python setup.py sdist
-python setup.py bdist_wheel
-pip install --upgrade dist/ibapi-10.25.1-py3-none-any.whl
+python setup.py install
 
 # Install sharadar_db_bundle and its requirements
 cd $PYTHON_LIBS
 git clone https://github.com/Moccazio/sharadar_db_bundle
-#git clone  https://github.com/alphaville76/sharadar_db_bundle.git
 cd $PYTHON_LIBS/sharadar_db_bundle
-pip install -r requirements.txt
-python setup.py sdist
-python setup.py bdist_wheel
-pip install --upgrade --force-reinstall dist/sharadar_db_bundle-2.0-py3-none-any.whl
+pip install -r requirements.txt --upgrade --no-deps
+python setup.py install
 
 if [ "$?" -eq 0 ]
 then
 echo "INSTALLATION SUCCESSFUL"
 else
 echo "INSTALLATION FAILED"
-source deactivate py39
-conda remove --name py39 --all -y
+conda remove --name py310 --all -y
 fi
+
