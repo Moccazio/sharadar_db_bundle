6c7208edb877cc6de1639ea060505e8ca41ad9cd
Maximilian Wimmer <71945683+Moccazio@users.noreply.github.com>
2024-06-26 16:17:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #conda create -n py39 -y -c conda-forge python=3.9 pandas numpy
-conda create -n py39 python=3.9 pandas numpy jupyterlab jupyter notebook -y
+conda create -n py39 python=3.9 pandas numpy jupyterlab jupyter notebook logbook -y
 source activate py39
 
 # mkdir ~/python_libs
@@ -18,14 +18,7 @@ rm ta-lib-0.4.0-src.tar.gz
 cd ta-lib
 ./configure
 make
-#sudo make install
-
-# Install zipline-reloaded with source code
-pip install zipline-reloaded # for dependecies
-git clone git@github.com:stefan-jansen/zipline-reloaded.git
-cd zipline-reloaded
-python setup.py build_ext --inplace
-python setup.py install
+sudo make install
 
 # Install TWS api
 cd $PYTHON_LIBS
@@ -33,7 +26,10 @@ wget https://interactivebrokers.github.io/downloads/twsapi_macunix.1025.01.zip
 unzip twsapi_macunix.1025.01.zip -d twsapi
 rm twsapi_macunix.1025.01.zip
 cd twsapi/IBJts/source/pythonclient
-python setup.py install
+pip install wheel
+python setup.py sdist
+python setup.py bdist_wheel
+pip install --upgrade dist/ibapi-10.25.1-py3-none-any.whl
 
 # Install sharadar_db_bundle and its requirements
 cd $PYTHON_LIBS
@@ -41,7 +37,9 @@ git clone https://github.com/Moccazio/sharadar_db_bundle
 #git clone  https://github.com/alphaville76/sharadar_db_bundle.git
 cd $PYTHON_LIBS/sharadar_db_bundle
 pip install -r requirements.txt
-python setup.py install
+python setup.py sdist
+python setup.py bdist_wheel
+pip install --upgrade --force-reinstall dist/sharadar_db_bundle-2.0-py3-none-any.whl
 
 if [ "$?" -eq 0 ]
 then
