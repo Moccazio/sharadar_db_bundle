c6eb59a68e26f8b7af841a5f1cfc7542b428a402
Maximilian Wimmer <71945683+Moccazio@users.noreply.github.com>
2024-07-05 13:59:54 +0200

    Update sql_lite_daily_pricing_test.py

--- test/sql_lite_daily_pricing_test.py
+++ test/sql_lite_daily_pricing_test.py
@@ -3,7 +3,7 @@ from sharadar.data.sql_lite_daily_pricing import SQLiteDailyBarWriter, SQLiteDai
 import nasdaqdatalink
 from os import environ as env
 import pandas as pd
-from sharadar.loaders.ingest_sharadar import process_data_table
+from sharadar.loaders.ingest_sharadar import process_data_table, get_data
 from exchange_calendars import get_calendar
 
 from sharadar.util import nasdaqdatalink_util
@@ -14,31 +14,24 @@ from zipline.data.bar_reader import (
 
 def create_data_list(df, sharadar_metadata_df):
     data_list = []  # list to send to daily_bar_writer
-
     # iterate over all the securities and pack data and metadata for writing
     tickers = df['ticker'].unique()
-
     for ticker in tickers:
         df_ticker = df[df['ticker'] == ticker]
         df_ticker = df_ticker.sort_index()
-
         sharadar_metadata = sharadar_metadata_df.loc[ticker]
-
         sid = sharadar_metadata.loc['permaticker']
-
         # drop metadata columns
         df_ticker = df_ticker.drop(['ticker', 'dividends'], axis=1)
-
         # pack data to be written by daily_bar_writer
         data_list.append((sid, df_ticker))
     return data_list
 
-
 nasdaqdatalink.ApiConfig.api_key=env["NASDAQ_API_KEY"]
 
-sharadar_metadata_df = nasdaqdatalink.get_table('SHARADAR/TICKERS', table='SEP', paginate=True)
+#sharadar_metadata_df = nasdaqdatalink.get_table('SHARADAR/TICKERS', table='SEP', paginate=True)
+sharadar_metadata_df = nasdaqdatalink.get_table('SHARADAR/TICKERS', table=['SFP', 'SEP'], paginate=True)
 sharadar_metadata_df.set_index('ticker', inplace=True)
-
 related_tickers = sharadar_metadata_df['relatedtickers'].dropna()
 # Add a space at the begin and end of relatedtickers, search for ' TICKER '
 related_tickers = ' ' + related_tickers.astype(str) + ' '
@@ -46,30 +39,35 @@ related_tickers = ' ' + related_tickers.astype(str) + ' '
 def dt(s):
     return pd.to_datetime(s)
 
-start = dt('2019-04-16')
-end = dt('2019-04-22')
+start = dt('2024-01-16')
+end = dt('2024-04-22')
+
 
 # dataframe with sid insted of ticker
 #data = nasdaqdatalink.get_table('SHARADAR/SEP', date={'gte':start,'lte':end}, ticker=['AAPL', 'IBM', 'PINS'], paginate=True)
-data = nasdaqdatalink.get_table('SHARADAR/SEP', date={'gte':start,'lte':end}, paginate=True)
+#data = nasdaqdatalink.get_table('SHARADAR/SEP', date={'gte':start,'lte':end}, paginate=True)
 #data['sid'] = data['ticker'].apply(lambda x: sharadar_metadata_df.loc[x]['permaticker'])
-data['sid'] = data['ticker'].apply(lambda x: lookup_sid(sharadar_metadata_df, related_tickers, x))
-data = process_data_table(data)
-data = data.drop(['ticker'], axis=1)
-
-data.set_index(['date', 'sid'], inplace=True)
+data = get_data(sharadar_metadata_df, related_tickers, start=0, end=None)
+#data['sid'] = data['ticker'].apply(lambda x: lookup_sid(sharadar_metadata_df, related_tickers, x))
+# unknown sids are -1 instead of nan to preserve the integer type. Drop them.
+#unknown_sids = data[data['sid'] == -1]
+#data.drop(unknown_sids.index, inplace=True)
+#data = process_data_table(data)
+#data = data.drop(['ticker'], axis=1)
+#data.set_index(['date', 'sid'], inplace=True)
+#data = process_data_table(data)
 
 print(data)
 
 #data_list = create_data_list(data, sharadar_metadata_df)
-calendar = get_calendar("NYSE")
+calendar = get_calendar("XNYS")
 #path = '/tmp/test.bcolz'
 #writer = BcolzDailyBarWriter(path,calendar,start,end)
 #ctable = writer.write(data_list)
 #print(ctable.attrs)
 #equity_daily_reader = BcolzDailyBarReader(path)
 
-dbpath = '/tmp/prices_new.db'
+dbpath = '/tmp/assets-7.sqlite'
 sql_writer = SQLiteDailyBarWriter(dbpath, calendar)
 sql_writer.write(data)
 
