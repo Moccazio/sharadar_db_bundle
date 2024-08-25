8318d9b3d7ce6d2e3abf0bbd9036bbd6d2f4533a
Maximilian Wimmer <71945683+Moccazio@users.noreply.github.com>
2024-08-25 11:59:37 +0200

    Update test_days_from_filings.py

--- test/test_days_from_filings.py
+++ test/test_days_from_filings.py
@@ -14,8 +14,8 @@ bundle.asset_finder.retrieve_equities([199059, 199623])
 
 spe = make_pipeline_engine()
 
-pipe_start = pd.to_datetime('2020-02-03', utc=True)
-pipe_end = pd.to_datetime('2020-02-07', utc=True)
+pipe_start = pd.to_datetime('2020-02-03', utc=False)
+pipe_end = pd.to_datetime('2020-02-07', utc=False)
 
 universe = StaticAssets(symbols(['IBM', 'F', 'AAPL']))
 
