f5f2576e6a13762969633f2380cfe3b4535f5750
Maximilian Wimmer <71945683+Moccazio@users.noreply.github.com>
2024-09-12 00:27:57 +0200

    Update output_dir.py

--- sharadar/util/output_dir.py
+++ sharadar/util/output_dir.py
@@ -23,10 +23,22 @@ def get_data_dir():
 def get_cache_dir():
     return os.path.join(create_data_dir(SHARADAR_BUNDLE_NAME), SHARADAR_BUNDLE_DIR, 'cache')
 
+def get_directory_size(directory):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
 def clear_cache_dir():
     """
-    Clears the cache directory used by Sharadar.
+    Clears the cache directory used by Sharadar if it exceeds 10GB.
     """
     cache_dir = get_cache_dir()  # Get the path to the cache directory
-    shutil.rmtree(cache_dir)  # Remove the cache directory and all its contents
-    os.makedirs(cache_dir)  # Recreate the cache directory
+    cache_size = get_directory_size(cache_dir)  # Calculate the size of the cache directory
+
+    # Check if the cache directory size exceeds 10GB (10 * 1024 * 1024 * 1024 bytes)
+    if cache_size > 10 * 1024 * 1024 * 1024:
+        shutil.rmtree(cache_dir)  # Remove the cache directory and all its contents
+        os.makedirs(cache_dir)  # Recreate the cache directory
