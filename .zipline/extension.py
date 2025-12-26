6b15a8bc162f0c0a01ce7287a913f24f9b19ba5b
Maximilian Wimmer <71945683+Moccazio@users.noreply.github.com>
2025-12-26 14:25:09 +0100

    Update extension.py

--- .zipline/extension.py
+++ .zipline/extension.py
@@ -1,8 +1,7 @@
 # extension.py
-
+# monkey patch to handle 'latest' ingest dir name in Zipline bundles
 import pandas as pd
 from zipline.data.bundles import core as bundles_core
-
 # --- MONKEY PATCH START ---
 # Save the original function just in case
 _original_from_bundle_ingest_dirname = bundles_core.from_bundle_ingest_dirname
@@ -18,7 +17,7 @@ def patched_from_bundle_ingest_dirname(ingest_dir_name):
 # Apply the patch to the Zipline module
 bundles_core.from_bundle_ingest_dirname = patched_from_bundle_ingest_dirname
 # --- MONKEY PATCH END ---
-
+# sharadar bundle registration
 try:
     from zipline.data.bundles import register
     from zipline.finance import metrics
